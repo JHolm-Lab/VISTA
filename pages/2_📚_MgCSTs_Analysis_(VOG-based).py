5ab1b7f31ff7d11f6a80347aeec289c2acefb85b
Amaury Maros <maros.amaury@gmail.com>
2024-09-16 14:31:22 -0400

    updated by region viz'

--- pages/2_📚_MgCSTs_Analysis_(VOG-based).py
+++ pages/2_📚_MgCSTs_Analysis_(VOG-based).py
@@ -17,6 +17,41 @@ projects = pd.read_csv("data/VIRGO2_projects_anonymous.csv")
 ######################################## ----- DATA PROCESSING ----- #############################################
 
 mgcsts_samples_project = mgcsts_samples.merge(projects, on = "sampleID", how = "left")
+data_to_replace = {
+    "Chinese":"China",
+    "FIJI":"Fiji",
+    "Irish":"Ireland",
+    "PreSSMat":"Bangladesh",
+    "ZAPPS":"Zambia",
+    "LSVF":"USA - AL",
+    "Gates-Stanford":"USA - CA, AL",
+    "Gates-UMD":"USA - MD, AL",
+    "MMOTH":"USA - MD",
+    "STING":"USA - MD",
+    "Symptoms":"USA - AL",
+    "VMRC":"USA - AL",
+    "XPENN":"USA - PA"
+}
+
+study_order = ["mgCST", "Bangladesh", "China", "Fiji", "Ireland", "Zambia", "USA - AL", "USA - MD", "USA - PA", "USA - MD, AL", "USA - CA, AL"]
+
+color_pal = {
+  "China":"#FDAE61",
+  "Fiji":"#D73027",
+  "Bangladesh":"#F46D43",
+  "Ireland":"#4D9221", 
+  "Zambia":"#FEE090",
+  "USA - MD":"#C6DBEF",
+  "USA - AL":"#6BAED6",
+  "USA - MD, AL":"#4575B4",
+  "USA - CA, AL":"#08519C",
+  "USA - PA":"#ABD9E9"
+  }
+
+projects = projects.replace(data_to_replace)
+mgcsts_samples_project = mgcsts_samples.merge(projects, on = "sampleID", how = "left")
+df2 = mgcsts_samples_project.groupby(["Project", "mgCST"]).size().reset_index().pivot(columns='Project', index = 'mgCST', values =0).reset_index()
+df2 = df2.reindex(columns=study_order[::-1])
 
 # Calculate count_sample for each mgCST
 count_sample = mgcsts_samples['mgCST'].value_counts().reset_index()
@@ -48,7 +83,7 @@ with col1 :
         hover_data=['domTaxa'],
         color_discrete_map=color_map, 
         labels={"count_sample" : "Number of samples"},
-        title = "Number of samples on each MgCSTs - colored by mgCST"
+        title = "Distribution and prevalence of mgCSTs"
         )
 
     # Customize layout
@@ -71,14 +106,13 @@ with col1 :
 # Stacked barplot mgcsts - number of samples colored by project
 with col2 :
 
-    df2 = mgcsts_samples_project.groupby(["Project", "mgCST"]).size().reset_index().pivot(columns='Project', index = 'mgCST', values =0).reset_index()
-
     fig = px.bar(
         df2, 
         x='mgCST', 
         y=df2.drop('mgCST', axis=1).columns.values, 
         labels={'value' : 'Number of samples'},
-        title = f"Number of samples on each MgCSTs - colored by projects")
+        color_discrete_map=color_pal,
+        title = f"Distribution and prevalence of mgCSTs by region")
        
     fig.update_xaxes(tickmode='array', tickvals=mgcsts['mgCST'])
 
@@ -87,6 +121,12 @@ with col2 :
         tickangle=0,  # Change the angle of the tick labels
         tickfont=dict(size=10)  # Change the size of the tick labels
         ))
+    fig.update_layout(
+    legend=dict(
+        title = 'Region',
+        traceorder='reversed'
+        )
+    )
     
     st.plotly_chart(fig, use_container_width=True)
 
@@ -113,7 +153,7 @@ with col3 :
         color='mgCST',
         color_discrete_sequence=list(bubble_color),
         size='count',
-        title = "Number of samples in previous MgCST vs New MgCST")
+        title = "Distribution and prevalence of mgCSTs - Comparison between mgCST v1 and mgCST v2")
     
     fig.update_xaxes(tickmode='array', tickvals=mgcsts['mgCST'])
     fig.update_yaxes(tickmode='array', tickvals=np.arange(1,28))
